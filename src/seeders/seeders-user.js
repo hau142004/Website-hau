3c971b774c26222ab2f8a2a2e174e786891b99e1
hau142004 <vhau142004@gmail.com>
2025-04-08 16:07:27 +0700

    create databse with sequelize_cli,mysql,xampp

--- src/seeders/seeders-user.js
+++ src/seeders/seeders-user.js
@@ -2,12 +2,24 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    
+    // email: DataTypes.STRING,
+    // password: DataTypes.STRING,
+    // firstName: DataTypes.STRING,
+    // lastName: DataTypes.STRING,
+    //  address: DataTypes.STRING,
+    //  gender: DataTypes.BOOLEAN,
+    //  roleid: DataTypes.STRING
     return queryInterface.bulkInsert('Users', [
       {
+        email: 'admin@gmail.com',
+        password: '123456',
         firstName: 'John',
         lastName: 'Doe',
-        email: 'example@example.com',
+        address: 'USA',
+        gender: 1,
+        typeRole: 'ROLE',
+        keyRole:'R1',
+        
         createdAt: new Date(),
         updatedAt: new Date(),
       },
